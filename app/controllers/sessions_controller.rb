577417568f9a886377fa3d388b46d00b82e17f5b
Rinold Simon <crisrinold@gmail.com>
2014-05-12 22:09:59 +0530

    changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,24 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_path
+    user = request.env["omniauth.auth"]
+	
+#	graph = Koala::Facebook::GraphAPI.new(@access)
+#friends = graph.get_connections("user", "friends")
+#graph.put_object("user", "feed", :message => "I am writing on my wall!")
+	
+@image =user["info"]["image"]
+@name=user["info"]["name"]
+  @access= user["credentials"]["token"]
+  @graph = Koala::Facebook::GraphAPI.new(@access)
+
+@graph.put_wall_post("test my fb page with ruby")
+#  @rest = Koala::Facebook::RestAPI.new(@access)
+
+#render :text => @graph.get_object("me")
+#render :text => @rest.get_object("me")
+
+   #session[:user_id] = user.id
+    #redirect_to root_path
   end
 
   def destroy
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1444532309127731', '3c163facc206f9180566f1e5cd29411c', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, '1444532309127731', '3c163facc206f9180566f1e5cd29411c',:scope => 'email,user_birthday,publish_actions', :image => 'access_token.info.image', :image_size => 'small'
 end
